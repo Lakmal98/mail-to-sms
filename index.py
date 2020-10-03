65f4c042bdd69c4078a9e255a87a25d80372e55d
Dimuthu Lakmal <39994919+Lakmal98@users.noreply.github.com>
2020-10-04 00:39:29 +0530

    Add else statement

--- index.py
+++ index.py
@@ -20,6 +20,7 @@ def main():
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+    else print("No token Exits")
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
